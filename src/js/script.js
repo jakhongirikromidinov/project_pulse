730a5c721075662f9a1f2fef468ccfec244b3e17
Jakhongir <jakhongir.ikromidinov@gmail.com>
2022-03-22 19:32:29 +0500

    slider

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/right.png"></button>' 
+    });
+});
